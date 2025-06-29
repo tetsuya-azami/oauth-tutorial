58153960f3f8d173ef3523c353776aa681e8ad48
tetsuya-azami <zqianjian35@gmail.com>
2025-06-29 16:20:56 +0900

    ユーザー、クライアント、認証コード、トークンのフィールドをプライベートに変更し、アクセサメソッドを追加

--- internal/domain/model.go
+++ internal/domain/model.go
@@ -1,31 +1,49 @@
 package domain
 
-// User represents a resource owner
-// TODO: 必要に応じて拡張
-
 type User struct {
-	ID   string
-	Name string
+	userID   string
+	loginID  string
+	password string
 }
+
+func (u *User) UserID() string   { return u.userID }
+func (u *User) LoginID() string  { return u.loginID }
+func (u *User) Password() string { return u.password }
 
 type Client struct {
-	ID          string
-	Secret      string
-	RedirectURI string
+	clientID    string
+	secret      string
+	redirectURI string
 }
+
+func (c *Client) ClientID() string    { return c.clientID }
+func (c *Client) Secret() string      { return c.secret }
+func (c *Client) RedirectURI() string { return c.redirectURI }
 
 type AuthCode struct {
-	Code      string
-	UserID    string
-	ClientID  string
-	ExpiresAt int64
-	State     string
+	code      string
+	userID    string
+	clientID  string
+	expiresAt int64
+	state     string
 }
 
+func (a *AuthCode) Code() string     { return a.code }
+func (a *AuthCode) UserID() string   { return a.userID }
+func (a *AuthCode) ClientID() string { return a.clientID }
+func (a *AuthCode) ExpiresAt() int64 { return a.expiresAt }
+func (a *AuthCode) State() string    { return a.state }
+
 type Token struct {
-	AccessToken  string
-	RefreshToken string
-	UserID       string
-	ClientID     string
-	ExpiresAt    int64
+	accessToken  string
+	refreshToken string
+	userID       string
+	clientID     string
+	expiresAt    int64
 }
+
+func (t *Token) AccessToken() string  { return t.accessToken }
+func (t *Token) RefreshToken() string { return t.refreshToken }
+func (t *Token) UserID() string       { return t.userID }
+func (t *Token) ClientID() string     { return t.clientID }
+func (t *Token) ExpiresAt() int64     { return t.expiresAt }
